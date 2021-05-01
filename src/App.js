0de0136bd96c74b71801890455a992b16d2af89c
Deborah Warren <Deborah.dw.32@gmail.com>
2021-05-01 06:16:59 -0400

    components and styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react"
+import "./App.css"
+import axios from 'axios'
+import Details from './Details'
+import styled from 'styled-components'
+import './index.css'
+
 
 function App() {
-  return (
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=LWkgHkyctCp0V5OpOSovstbd3ma0qB8ct39k9DQv')
+      .then(response => {
+        console.log(response)
+        setData(response.data)
+      })
+      .catch(err => {
+        console.log('Error Caught: ', err)
+      })
+    },[])
+  
+    return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <div className="header">
+        <PageTitle>NASA Photo of the Day</PageTitle>
+      </div>
+      {
+        <Details title={data.title} url={data.url} copyright={data.copyright} date={data.date} explanation={data.explanation}/>
+      }
     </div>
   );
 }
 
-export default App;
+const PageTitle = styled.h1`
+font-size: 3em;
+font-family: 'Zen Dots', cursive;
+color: #FAEEF5;
+text-align: center;
+`
+
+
+export default App
